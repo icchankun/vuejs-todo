5ef273a1b5574fb5fe012bd92d854b06f021851e
icchankun <ikkun.official@gmail.com>
2024-05-22 10:25:55 +0900

    新規ToDoと既存ToDoのidが重複することを防ぐため、ブラウザをリロードしてもidが0に戻らないようにする。

--- todo.js
+++ todo.js
@@ -2,15 +2,25 @@ import { createApp, ref, onMounted } from "vue";
 
 const app = createApp({
   setup() {
-    let id = 0;
-
     const newTodoText = ref("");
     const todos = ref([]);
 
+    const fetchSavedId = () => {
+      const savedId = localStorage.getItem("id");
+      return savedId ? JSON.parse(savedId) : 0;
+    };
+
+    const id = ref(fetchSavedId());
+
     const addTodo = () => {
-      todos.value.push({ id: id++, text: newTodoText.value, done: false });
+      todos.value.push({
+        id: ++id.value,
+        text: newTodoText.value,
+        done: false,
+      });
       newTodoText.value = "";
       localStorage.setItem("todos", JSON.stringify(todos.value));
+      localStorage.setItem("id", todos.value.at(-1).id);
     };
 
     const deleteTodo = (selectedTodo) => {
